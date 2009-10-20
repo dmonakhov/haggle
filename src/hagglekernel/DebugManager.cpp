cf3a4ad404164a5f5c91a6685a5b514295e42376
daniel aldman <daniel.aldman@it.uu.se>
2009-10-20 14:18:55 +0200

    Added colons in two log messages, to ease parsing.

--- src/hagglekernel/DebugManager.cpp
+++ src/hagglekernel/DebugManager.cpp
@@ -377,7 +377,7 @@ void DebugManager::onDebugReport(Event *e)
 {
 	//kernel->getInterfaceStore()->print();
 	
-	LOG_ADD("%s kernel event queue size=%u\n", Timeval::now().getAsString().c_str(), kernel->getSize()); 
+	LOG_ADD("%s: kernel event queue size=%u\n", Timeval::now().getAsString().c_str(), kernel->getSize()); 
 	kernel->getNodeStore()->print();
 #ifdef DEBUG_DATASTORE
 	kernel->getDataStore()->print();
