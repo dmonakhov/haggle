11f1c7dd6aa42488dcf5930e90123db66f6b4267
Erik Nordström <erik.nordstrom@gmail.com>
2010-04-04 15:39:49 -0400

    - Updated android install script and set adhoc mode script to   work better with nexus one

--- android/device-files/adhoc.sh
+++ android/device-files/adhoc.sh
@@ -4,25 +4,49 @@
 # Usage: adhoc.sh [start|stop] nodeNR
 
 NODE=10
+NETDEV=eth0
+MODULE_PATH=/system/lib/modules
+POST_INSERT_CMD=
+
+if [ -f $MODULE_PATH/wlan.ko ]; then
+    NETDEV=tiwlan0
+    MODULE=wlan
+    HOSTNAME=Magic
+    POST_INSERT_CMD="wlan_loader -f /system/etc/wifi/Fw1251r1c.bin -e /proc/calibration -i /data/local/tiwlan.ini"
+elif [ -f $MODULE_PATH/bcm4329.ko ]; then
+    NETDEV=eth0
+    MODULE=bcm4329
+    HOSTNAME=NexusOne
+    POST_INSERT_CMD="iwconfig eth0 mode ad-hoc essid HaggleHoc"
+else
+    echo "Unknown network device..."
+    exit
+fi
 
 case "$1" in
 'start')
 if [ ! -z $2 ]; then
     NODE=$2
 fi
-echo "Node IP is 192.168.0.$NODE"
-hostname "android.uu$NODE"
-insmod /system/lib/modules/wlan.ko
-wlan_loader -f /system/etc/wifi/Fw1251r1c.bin -e /proc/calibration -i /data/local/tiwlan.ini
-ifconfig tiwlan0 192.168.0.$NODE netmask 255.255.255.0
-ifconfig tiwlan0 up 
+
+echo "Node IP is 192.168.2.$NODE"
+
+hostname "$HOSTNAME-$NODE"
+insmod $MODULE_PATH/$MODULE.ko
+
+if [ ! -z $POST_INSERT_CMD ]; then
+    eval $POST_INSERT_CMD
+fi
+
+ifconfig $NETDEV 192.168.2.$NODE netmask 255.255.255.0
+ifconfig $NETDEV up 
 
 ;;
 'stop')
 
-if grep wlan /proc/modules 2>/dev/null; then
-    ifconfig tiwlan0 down
-    rmmod wlan
+if grep $MODULE /proc/modules 2>/dev/null; then
+    ifconfig $NETDEV down
+    rmmod $MODULE
 fi
 ;;
 *)
