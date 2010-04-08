b82bd8a1ab5091f5fb820848d65214cc05d12d5c
Erik Nordström <erik.nordstrom@gmail.com>
2010-04-07 21:12:04 -0400

    * Fixed segfault when parsing routing information data object

--- src/hagglekernel/ForwardingManager.cpp
+++ src/hagglekernel/ForwardingManager.cpp
@@ -784,10 +784,7 @@ void ForwardingManager::findMatchingDataObjectsAndTargets(NodeRef& node)
 
 size_t ForwardingManager::fromTriggerListMetadata(Metadata *m, NodeRefList& trigger_list)
 {	
-	
-	HAGGLE_DBG("Parsing triggerlist\n");
-
-	if (m->getName() != "TriggerList")
+	if (!m || m->getName() != "TriggerList")
 		return 0;
 		
 	Metadata *tm = m->getMetadata("Node");
