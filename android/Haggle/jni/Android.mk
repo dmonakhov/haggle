037c5f05d648e17eaf948b2950290c800fd46a38
Erik Nordström <erik.nordstrom@gmail.com>
2011-08-22 08:38:13 -0400

    Android.mk changes to produce statically linked apps for Android.

--- android/Haggle/jni/Android.mk
+++ android/Haggle/jni/Android.mk
@@ -9,11 +9,3 @@ subdirs := $(addprefix $(APP_PROJECT_PATH)/../../,$(addsuffix /Android.mk, \
 EXTRA_LIBS_PATH := $(APP_PROJECT_PATH)/../extlibs/external
 
 include $(subdirs)
-
-#$(call __ndk_info,$(LOCAL_MAKEFILE):$(LOCAL_MODULE): subdirs are : $(subdirs))
-
-#$(call import-module,sqlite/dist)
-#$(call import-module,dbus)
-#$(call import-module,bluetooth/bluez)
-#$(call import-module,zlib)
-#$(call import-module,openssl/crypto)
